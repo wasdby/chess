055e74bf6785d6ed1b2c017cb57f4c951ba53e9b
Chamber6821 <vania.osinin@yandex.ru>
2025-01-26 16:58:38 +0300

    Implement Nothing

---
+++ src/components/atoms/Nothing/index.d.ts
@@ -0,0 +1,9 @@
+import { Component, Container } from "../../component"
+
+/**
+ * Олицетворение паттерна NullObject
+ */
+export class Nothing implements Component {
+  mount(container: Container): void
+  unmount(): void
+}
---
+++ src/components/atoms/Nothing/index.js
@@ -0,0 +1,4 @@
+export class Nothing {
+  mount() { }
+  unmount() { }
+}
