9dac868c6ebf0dea7ea30345cd9ed7c8498a488b
Chamber6821 <vania.osinin@yandex.ru>
2025-01-26 17:32:07 +0300

    Draft Authentication page

---
+++ src/components/pages/Authentication/components/Header/index.d.ts
@@ -0,0 +1,10 @@
+import { Component, Container } from "../../../../component"
+
+/**
+ * Заголовки РЕГИСТРАЦИЯ/ВХОД/ВОССТАНОВЛЕНИЕ ПАРОЛЯ/НОВЫЙ ПАРОЛЬ 
+ */
+export class Header implements Component {
+  constructor(text: string)
+  mount(container: Container): void
+  unmount(): void
+}
---
+++ src/components/pages/Authentication/components/Input/index.d.ts
@@ -0,0 +1,18 @@
+import { Component, Container } from "../../../../component"
+
+/**
+ * Поле ввода для логина/почты/пароля/кода
+ */
+export class Input implements Component {
+  constructor(
+    options: {
+      placeholder: string,
+      rightDecorator?: Component
+      onChanged?: (text: string) => void
+    }
+  )
+  mount(container: Container): void
+  unmount(): void
+  get text(): string
+  set text(text: string)
+}
