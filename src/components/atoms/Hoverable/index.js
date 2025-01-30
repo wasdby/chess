df479a8e6500470c8c31f56949f0713da962e27a
Pobortsev A. <pobortsev03@gmail.com>
2025-01-30 15:35:10 +0300

    implement atoms-hoverable

---
+++ src/components/atoms/Hoverable/index.js
@@ -0,0 +1,25 @@
+export class Hoverable {
+  constructor (content, options = {}) {
+    this.content = content
+    this.element = document.createElement('div')
+
+    if (options.onEnter) {
+      this.element.addEventListener('mouseenter', () => options.onEnter(this.content))
+    }
+
+    if (options.onLeave) {
+      this.element.addEventListener('mouseleave', () => options.onLeave(this.content))
+    }
+  }
+
+  mount (container) {
+    container.appendChild(this.element)
+    this.content.mount(this.element)
+  }
+
+  unmount () {
+    this.element.parentElement.removeChild(this.element)
+  }
+}
+
+export default Hoverable
