eed9f10ba1db3f61587a2b3935d4129ab86c94b3
Shupenko Anton <77282691+wasdby@users.noreply.github.com>
2025-01-31 19:28:22 +0300

    fix: wrapper, contain in Clickable

--- src/components/atoms/Clickable/index.js
+++ src/components/atoms/Clickable/index.js
@@ -2,16 +2,12 @@ export class Clickable {
   constructor (content, options) {
     this.content = content
     this.options = options
-    this.container = null
-    this.wrapper = null
+    this.wrapper = document.createElement('div')
+    this.wrapper.className = 'clickable'
   }
 
   mount (container) {
-    this.container = container
-    this.wrapper = document.createElement('div')
-    this.wrapper.className = 'clickable'
     this.content.mount(this.wrapper)
-
     container.appendChild(this.wrapper)
     this.wrapper.addEventListener('click', () => this.options.onClick(this.content))
   }
@@ -19,6 +15,8 @@ export class Clickable {
   unmount () {
     this.wrapper.removeEventListener('click', () => this.options.onClick(this.content))
     this.content.unmount()
-    this.container.removeChild(this.wrapper)
+    if (this.wrapper.parentElement) {
+      this.wrapper.parentElement.removeChild(this.wrapper)
+    }
   }
 }
