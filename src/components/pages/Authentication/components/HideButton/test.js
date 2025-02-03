b8d2c1d097764da0687f3d692d28eabb96e41110
Pobortsev A. <pobortsev03@gmail.com>
2025-02-03 19:46:17 +0300

    fix circular dependencies

--- src/components/pages/Authentication/components/HideButton/test.js
+++ src/components/pages/Authentication/components/HideButton/test.js
@@ -1,27 +1,17 @@
-import { Input } from '../Input/index.js'
 import { HideButton } from './index.js'
 
 const root = document.getElementById('root')
 
 const showBtn = new HideButton(handleButtonClick)
-const placeholder = 'input'
-const input = new Input({
-  placeholder,
-  onChanged: (text) => {
-    toggleVisibility(showBtn.show)
-  }
-})
 
 function handleButtonClick () {
   showBtn.show = !showBtn.show
   toggleVisibility(showBtn.show)
 }
-
-input.mount(root)
-
+document.createElement('input')
 showBtn.mount(root)
 
-const inputField = root.querySelector('.pages-input-wrapper').querySelector('input')
+const inputField = root.querySelector('input')
 function toggleVisibility (value) {
   if (value) {
     inputField.type = 'password'
