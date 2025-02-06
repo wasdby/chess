7b8f3cef67f94d12b8dcf81278c871f5f5728d49
Pobortsev A. <pobortsev03@gmail.com>
2025-02-06 23:47:38 +0300

    fix content mounting

--- src/components/common/Slot/index.js
+++ src/components/common/Slot/index.js
@@ -1,24 +1,26 @@
 export class Slot {
   constructor (content) {
     this.element = content
-    this.slot = document.createElement('div')
-    this.slot.className = 'common-slot'
-    // this.slot = new Raw('<div class="common-slot"></div>')
+    // this.slot = document.createElement('div')
+    // this.slot.className = 'common-slot'
   }
 
   mount (container) {
-    container.appendChild(this.slot)
-    this.element.mount(this.slot)
+    // container.appendChild(this.slot)
+    // this.element.mount(this.slot)
+    this.container = container
+    this.element.mount(container)
   }
 
   unmount () {
     this.element.unmount()
-    this.slot.parentElement.removeChild(this.slot)
+    // this.slot.parentElement.removeChild(this.slot)
   }
 
   replace (content) {
     this.element.unmount()
     this.element = content
-    this.element.mount(this.slot)
+    this.element.mount(this.container)
+    // this.element.mount(this.slot)
   }
 }
