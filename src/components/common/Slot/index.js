ed73af7051738fc296fb11d74c0e1fa2d6c78e37
Pobortsev A. <pobortsev03@gmail.com>
2025-02-06 23:51:50 +0300

    fix unmount

--- src/components/common/Slot/index.js
+++ src/components/common/Slot/index.js
@@ -1,26 +1,21 @@
 export class Slot {
   constructor (content) {
     this.element = content
-    // this.slot = document.createElement('div')
-    // this.slot.className = 'common-slot'
   }
 
   mount (container) {
-    // container.appendChild(this.slot)
-    // this.element.mount(this.slot)
     this.container = container
     this.element.mount(container)
   }
 
   unmount () {
     this.element.unmount()
-    // this.slot.parentElement.removeChild(this.slot)
+    this.container = undefined
   }
 
   replace (content) {
     this.element.unmount()
     this.element = content
     this.element.mount(this.container)
-    // this.element.mount(this.slot)
   }
 }
