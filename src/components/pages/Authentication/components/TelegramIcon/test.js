f98ddc7768bd1792128f670422543fe7b75cd649
Pobortsev A. <pobortsev03@gmail.com>
2025-02-10 21:52:08 +0300

    fix telegram link

--- src/components/pages/Authentication/components/TelegramIcon/test.js
+++ src/components/pages/Authentication/components/TelegramIcon/test.js
@@ -2,23 +2,19 @@ import { Wrap } from '../../../../atoms/Wrap/index.js'
 import { List } from '../../../../common/List/index.js'
 import { Label } from '../Label/index.js'
 import { TelegramIcon } from './index.js'
-import { Clickable } from '../../../../atoms/Clickable/index.js'
 import { Background } from '../Background/index.js'
 const root = document.getElementById('root')
 
-const tgLink = new Clickable(new Wrap(new List(
+const tgLink = new Wrap(new List(
   new Label('Чат игроков'),
   new TelegramIcon()
 ), {
   wrap: (container) => {
-    const linkDiv = document.createElement('div')
+    const linkDiv = document.createElement('a')
     linkDiv.className = 'pages-tg-link'
+    linkDiv.href = 'https://web.telegram.org'
     container.appendChild(linkDiv)
     return linkDiv
-  }
-}), {
-  onClick: () => {
-    window.location.href = 'https://web.telegram.org/a/'
   }
 })
 
